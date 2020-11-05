2c85118c946c2e1c41a6448db49063d063c32823
shubham-natekar <shubham.natekar@greenpatheducation.com>
2020-11-05 19:18:33 +0530

    esha confilts

--- Esha/text_to_audio.py
+++ Esha/text_to_audio.py
@@ -6,18 +6,13 @@ input_file = open('ucharan.txt', 'r')
 count_lines = 0
 for line in input_file:
     print (line)
-    if line == '\t' or line == '\n':
-        break
-    
-    else:  
-        myobj= gTTS(text=line, lang='hi', slow=False)
-    #file_input = input("Enter a file name  : ")
+    break if (line == '\t' or line == '\n') else myobj= gTTS(text=line, lang='hi', slow=False)
         count_lines += 1
         fname="ucharan_"+str(count_lines)+".mp3"
         myobj.save(fname)
    # myobj.save("audio_file.mp3")
         os.system("mpg321 fname")    
-        
+    print(line)        
 print ('number of lines:', count_lines)
 input_file.close()
 
