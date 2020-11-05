da43dcc2eb7e6c6491a9f60ea61a866c11e8d3fa
rakesh-singh15 <72030939+rakesh-singh15@users.noreply.github.com>
2020-11-05 19:05:31 +0530

    Update cyclically_rotation.py

--- rakesh/cyclically_rotation.py
+++ rakesh/cyclically_rotation.py
@@ -7,7 +7,8 @@ def cyclically(lst,d,n):
 		lst1.append(lst[i])
 	lst2 = [i for i in lst + lst1 if i not in lst or i not in lst1]
 	lst=lst2 + lst1
-	print(lst)
+	return lst
+	#print(lst)
 
 lst=[]
 n=int(input("How many number:"))
@@ -16,3 +17,4 @@ for i in range(n):
 	ele=int(input())
 	lst.append(ele)
 cyclically(lst,d,n)
+print(lst)
