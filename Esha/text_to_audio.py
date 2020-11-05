5769d14a7e0d7eb86db675425a0c6fe22533d2e3
shubham-natekar <shubham.natekar@greenpatheducation.com>
2020-11-05 19:33:26 +0530

    code updated by shubham

--- Esha/text_to_audio.py
+++ Esha/text_to_audio.py
@@ -6,17 +6,9 @@ input_file = open('ucharan.txt', 'r')
 count_lines = 0
 for line in input_file:
     print (line)
-    if line == '\t' or line == '\n':
-        break
-    
-    #file_input = input("Enter a file name  : ")
+    break if line == '\t' or line == '\n' else continue 
         count_lines += 1
         fname="ucharan_"+str(count_lines)+".mp3"
         myobj.save(fname)
-   # myobj.save("audio_file.mp3")
-        os.system("mpg321 fname")    
-    print(line)        
-print ('number of lines:', count_lines)
-input_file.close()
-
-print("for conflicts")
+        print("nothing to commit")
+for j in range 
